8a8957cb4f4a8828127074eded33277002fe7aa8
Mingyue Li <3249977074@qq.com>
2025-06-14 11:48:14 +0800

    vault backup: 2025-06-14 11:48:14 M README.md quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Coral's Notes",
+    pageTitleSuffix: "微雨，扶桑树下的小屋",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    // baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
